2a44796b28bdf0be7f77d5fed443d4eb2c5fd351
leenajo <leenajo@naver.com>
2020-05-19 16:19:36 +0900

    init

---
+++ src/test/java/com/bootstudy/springwebservice/SpringWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootstudy.springwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
